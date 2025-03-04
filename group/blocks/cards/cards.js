2b872ff0dd53f76b7e43f423f4315c9693797cd4
Rob Rusher <rrusher@adobe.com>
2025-03-04 14:43:43 -0700

    reorg

--- group/blocks/cards/cards.js
+++ group/blocks/cards/cards.js
@@ -7,25 +7,8 @@ export default function decorate(block) {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      const link = div.querySelectorAll('a[href]');
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else if (div.children.length === 1 && div.querySelector('p')) div.className = 'cards-card-title';
       else div.className = 'cards-card-body';
-      if (link.length > 1) {
-        link.forEach((a) => {
-          const divClone = div.cloneNode(true);
-          a.append(divClone);
-          const liClone = li.cloneNode(true);
-          liClone.append(a);
-          ul.append(liClone);
-        });
-      } else if (link) {
-        const a = document.createElement('a');
-        a.href = link[0].href;
-        link[0].parentNode.remove();
-        a.append(div);
-        li.append(a);
-      }
     });
 
     ul.append(li);
