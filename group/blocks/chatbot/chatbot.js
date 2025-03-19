2cc4aabd997676e24714755c519cd837ade9e988
Rob Rusher <rrusher@adobe.com>
2025-03-19 11:49:59 -0600

    rework for iframe

--- group/blocks/chatbot/chatbot.js
+++ group/blocks/chatbot/chatbot.js
@@ -1,5 +1,3 @@
-import { loadCSS } from '../../scripts/aem.js';
-
 /**
  * Transforms the API response JSON into the required format
  * @param {Object} response - The original JSON response from the API
@@ -8,23 +6,23 @@ import { loadCSS } from '../../scripts/aem.js';
 function transformJson(response) {
   const result = {};
   response.data.forEach((item) => {
-    const { key, name, network_id } = item;
+    const { key, name, network_id: networkId } = item;
     if (key === 'plans' || key === 'dental_plans' || key === 'vision_plans') {
-      // Initialize the array if it doesn't exist
       if (!result[key]) {
         result[key] = [];
       }
       let networkIdArray = [];
       try {
-        if (network_id && network_id !== '') {
-          networkIdArray = JSON.parse(network_id);
+        if (networkId && networkId !== '') {
+          networkIdArray = JSON.parse(networkId);
         }
       } catch (e) {
-        console.error(`Error parsing network_id for ${name}:`, e);
+        // eslint-disable-next-line no-console
+        console.error(`Error parsing networkId for ${name}:`, e);
       }
       result[key].push({
         name,
-        network_id: networkIdArray,
+        networkId: networkIdArray,
       });
     } else {
       result[key] = name;
@@ -34,25 +32,22 @@ function transformJson(response) {
 }
 
 export default function decorate(block) {
-  loadCSS('https://eva-us.app2check.com/app/plugins/eva/eva-bs.css');
-  // await the loading of the script
-  const evascript = document.createElement('script');
-  evascript.src = 'https://eva-us.app2check.com/app/plugins/eva/eva-bs.js';
-  block.append(evascript);
   const path = window.location.pathname.split('/').slice(0, -1).join('/');
-
-  evascript.onload = () => {
-    fetch(`${path}/eva.json`)
-      .then((response) => response.json())
-      .then((config) => {
-        const qparam = JSON.stringify(transformJson(config));
-        const script = document.createElement('script');
-        script.textContent = `eva_Init("idConverse=60e720e7-e522-4d54-b034-df2ed65e0d5e", true, 500, 600, "https://eov-assets.s3.amazonaws.com/blue_shield_california_onboarding/fad_icon.png", ${qparam});`;
-        block.append(script);
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.error('Failed to load chatbot configuration', error);
-      });
-  };
+  fetch(`${path}/eva.json`)
+    .then((response) => response.json())
+    .then((config) => {
+      const qparam = JSON.stringify(transformJson(config));
+      const evaURL = 'https://eva-us.app2check.com/app/chat2check/bots/chatbotWebClient-v5-bs.html';
+      const encodedParams = btoa(qparam.toString());
+      const iframe = document.createElement('iframe');
+      iframe.src = `${evaURL}?params=${encodedParams}`;
+      iframe.width = '100%';
+      iframe.height = '400';
+      iframe.style = 'border: none;';
+      block.append(iframe);
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Failed to load chatbot configuration', error);
+    });
 }
