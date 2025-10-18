18e8486512acd79bb322a0c014e6f7198a6c3cad
mattsd23-bsc <matthew.sorensen@blueshieldca.com>
2025-10-18 16:51:59 -0700

    Update scripts.js

--- group/scripts/scripts.js
+++ group/scripts/scripts.js
@@ -305,7 +305,6 @@ function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
   window.setTimeout(() => import('./delayed.js'), 3000);
   // load anything that can be postponed to the latest here
-  
 }
 
 async function loadPage() {
