45981228e5b75b626057669a513f7dd814e52a3f
mattsd23-bsc <matthew.sorensen@blueshieldca.com>
2026-01-13 16:12:38 -0800

    Update scripts.js

--- group/scripts/scripts.js
+++ group/scripts/scripts.js
@@ -301,7 +301,7 @@ async function loadLazy(doc) {
  */
 function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 1500);
+  window.setTimeout(() => import('./delayed.js'), 2000);
   // load anything that can be postponed to the latest here
 }
 
