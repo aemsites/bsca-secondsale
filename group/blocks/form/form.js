a97f0c35654e6fc98d3c1b521f0f6c4e3ecb2250
mattsd23-bsc <matthew.sorensen@blueshieldca.com>
2026-05-13 18:02:37 -0700

    Fix confirmation page URL format

--- group/blocks/form/form.js
+++ group/blocks/form/form.js
@@ -4,7 +4,7 @@ export default function decorate(block) {
   const endpoint = endpointLink?.href || block.textContent.trim();
 
   // TODO: Update this URL once the confirmation page is live in EDS
-  const confirmationPageUrl = '/group/oc/county-of-orange-confirmation-2025';
+  const confirmationPageUrl = '/group/oc/county_of_orange_confirmation_2025';
 
   // Clear authored content once we have what we need
   block.textContent = '';
